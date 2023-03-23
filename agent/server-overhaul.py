f2adbbc8a3c404aed53e95594525315d90711c95
ryan kleffman <ryankleffman47@gmail.com>
2023-03-23 14:20:28 -0500

    added some more error handling

--- agent/server-overhaul.py
+++ agent/server-overhaul.py
@@ -10,20 +10,26 @@ from datetime import datetime, timezone
 import select
 import logging
 
+"""
+Here's the global Debug + Logging settings. 
+Global Debug print to screen will be a setting in the future
+"""
+global_debug = True
 
 ##Reference: https://realpython.com/python-logging/
 logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(filename='server.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', force=True)
-#logging.debug('This will get logged')
+## Change the path to the system path + a log folder/file somewhere
+logging.basicConfig(filename='../server.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', force=True)
 
-global_debug = True
+if global_debug:
+    logging.getLogger().addHandler(logging.StreamHandler())
+
 
 class ServerSockHandler:
     """
     Description: A class that handles the inital connections, and sends them
     to the other respective classes as needed
 
-    
     """
 
     def __init__(self):
@@ -32,6 +38,9 @@ class ServerSockHandler:
         ##Errors relevant to this funtion
         self.Sx01 = "conn_broken_pipe: A pipe was broken"
         self.Sx02 = "Socket Close: The socket was successfully closed"
+        self.Sx03 = "Socket Timeout: The socket timed out"
+        self.Sx04 = "Socket Permission Error: You most likely don'y have permissions to open a socket"
+        self.Sx05 = "Connection Refused, Reset or aborted: "
         self.Sx10 = "client_unkown_input: An unkown input was receieved from the client"
 
         self.SxXX = "Unkown error: "
@@ -41,8 +50,31 @@ class ServerSockHandler:
             self.server.close()
             logging.debug(self.Sx02)
 
+
         except Exception as e:
             logging.warning(f"{self.SxXX}:{e}")
+
+    def start_server(self, ip, port):
+        try:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # this allows the socket to be reelased immediatly on crash/exit
+            self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            self.ADDR = (ip,port)
+            self.server.bind(self.ADDR)  
+            atexit.register(self.socket_cleanup)
+
+        ##Exceptions: https://docs.python.org/3/library/exceptions.html#TimeoutError
+        except TimeoutError as e:
+            logging.warning(f"{self.Sx03}: \n ERRMSG: {e}\n")
+        except PermissionError as e:
+            logging.warning(f"{self.Sx04}: \nERRMSG:{e}\n")
+        except (ConnectionRefused, ErrorConnectionResetError, ConnectionAbortedError) as e:
+            logging.warning(f"{self.Sx05}: \nERRMSG:{e}\n")
+        except Exception as e:
+            logging.warning(f"{self.SXX}:ERRMSG: {e}\n")
+        
+
     
 
             
@@ -53,5 +85,5 @@ class ServerSockHandler:
 
 
 s = ServerSockHandler()
-s.socket_cleanup()
+s.start_server("127.0.0.1",80)
 logging.debug("ServerSockHandler Called")
