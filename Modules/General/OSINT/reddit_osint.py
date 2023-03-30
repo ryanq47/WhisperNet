95b5c7b6f51eacc0caa5d349432b12df6a64f95e
ryan kleffman <ryankleffman47@gmail.com>
2023-03-29 17:08:10 -1000

    added generalmap for refactor

---
+++ Modules/General/OSINT/reddit_osint.py
@@ -0,0 +1,16 @@
+"""
+New Reddit Osint
+
+Mapping:
+
+Gui -> RedditOsint Class
+
+reddit_framework(self, credentials:list, search_list:list)
+    Oauth
+    Search
+    Parse
+
+return results to GUI & writeto DB from there
+
+
+"""
