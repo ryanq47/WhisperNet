d3229fad4b00da58002a5ecc9cf1277d11649408
ryanq47 <ryankleffman47@gmail.com>
2023-04-06 11:55:21 -0500

    started expanding new reddit osing stuff

--- Modules/General/OSINT/reddit_osint.py
+++ Modules/General/OSINT/reddit_osint.py
@@ -21,6 +21,7 @@ return results to GUI & writeto DB from there
 """
 import requests
 import logging
+import random
 from PySide6.QtCore import QThread, Signal, QObject, Slot
 
 global_debug = True
@@ -44,8 +45,8 @@ class OsintReddit:
         API
         """
         logging.debug(f"[Credentials] {self.credentials_list}")
-        ## Note, will be random in the future
-        self.user_agent = "TestBot 0.0.1"
+        ## Note, add optional set user agent in GUI options. Pass in via a list or something
+        self.user_agent = self.user_agent_generator()
 
         auth = requests.auth.HTTPBasicAuth(self.client_id, self.secret_token)
         data = {'grant_type': 'password',
@@ -103,4 +104,26 @@ class OsintReddit:
         ## so maybe emit one line, write (or append) said line, and return the next line and 
         ## so on? 
 
+    def url_legos():
+        """
+        Takes items from list, sets values for url pieces. Called legos cause it's creating little "lego" pieces 
+        to build with
+        """
+        pass
+    
+    def url_build():
+        """
+        Builds the URL with the previously created legos
+        """
+        pass
+    
+
+    def user_agent_generator() -> str:
+        ## Some random names, you can customize to whatever you want
+        agent = ["RedditBot", "SuperMan", "TMNT", "RareDonkey"]
+        version_number = random.randomint(1,99)
+        
+        name = f"{random.choice(agent)} v{version_number}"
+        ## Returning name instead of self.name so self.name never is nothing
+        return name
         
