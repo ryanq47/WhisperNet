b6b043ce05e525b4196c762a41085c83ac05c3d3
ryanq47 <ryankleffman47@gmail.com>
2023-04-21 15:53:15 -0500

    deleted a character which shoudlny have been deleted

--- logec-attack.py
+++ logec-attack.py
@@ -91,7 +91,7 @@ logging.getLogger().addHandler(logging.StreamHandler())
 ####################
 
 class LogecSuite(QMainWindow, Ui_LogecC3):
-    def __init__(self:
+    def __init__(self):
         super(LogecSuite, self).__init__()
         self.setupUi(self)
         ##!! Dev Note, can possible wrap these in a try/except, would help with error handling/developing
