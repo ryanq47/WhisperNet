24f612a40912acca41f43662575c0d1162865adf
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2023-07-12 22:15:04 -0500

    Update
    Added Comms\JsonHandler

---
+++ Agent/c#/Comms/JsonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Comms
+{
+    //using internal so only the assmbly can access it. Might need to tighten down later
+    internal class JsonHandler
+    {
+        internal static void Test()
+        {
+            Console.WriteLine("\tJsonHandler.CS: Success.");
+        }
+    }
+}
