020fcec032bb52ca7205b4b2046857bf4bc34808
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2023-08-07 09:43:13 -0700

    Client dev

---
+++ Client/Utils/AuthenticationHandler.py
@@ -0,0 +1,52 @@
+
+try:
+    from getpass import getpass
+
+except Exception as e:
+    print(f"[client.Utils.CredenialHandler] Import Error: {e}")
+    exit()
+
+class Credentials:
+    @staticmethod
+    def get_password():
+        password = getpass("Enter Password: ")
+        return password
+
+    @staticmethod
+    def get_username():
+        '''Doesn't need to exist, but makes the client.py a bit more readable
+        '''
+        username = input("Enter Username: ")
+        return username
+    
+    @staticmethod
+    def authenticate_to_server():
+        cookie = None
+        user = Credentials.get_username()
+        passwd = Credentials.get_password()
+
+        ## send request to server...
+
+        ## get cookie...
+        if not cookie:
+            print("Authentication Failed")
+
+        return cookie
+
+class Server:
+    @staticmethod
+    def get_server_to_connect_to():
+        '''Prompt user for the server IP & Port to connect to. Returns that data as a Tuple
+        
+        '''
+        server = input("Enter Server & port. Ex 127.0.0.1:5000: ")
+        try:
+            ip = server.split(":")[0]
+            port = server.split(":")[1]
+
+            server = [ip, port]
+            return server
+
+        except Exception as e:
+            print(f"Invalid input: {e}")
+            Server.get_server_to_connect_to()
---
+++ Client/client.py
@@ -0,0 +1,45 @@
+try:
+    import Logic.DecisionTree
+    import Utils.AuthenticationHandler
+
+except Exception as e:
+    print(f"[client.py] Import Error: {e}")
+    exit()
+
+class Client:
+    def __init__(self):
+        pass
+        self.cookie = None
+        ## the server to conenct to
+        self.server = None #[str, ip]
+
+    def user_loop(self):
+        while True:
+            ## this can be argparse as well, maybe move to a util
+            if self.server == None:
+                Utils.AuthenticationHandler.Server.get_server_to_connect_to()
+
+                    # recursive call this again
+
+            if self.cookie == None:
+                print(f"Enter credentials for {self.server}:")
+                Utils.AuthenticationHandler.Credentials.authenticate_to_server()
+
+
+        '''
+        ## not doing a persistent cnonection to server, just one off's
+        While True:
+            if self.cookie == None:
+                ask_for_creds()
+                Auth_to_server()
+
+            get_user_input()
+            decision_tree(user_input, cookie)
+
+        
+        
+        '''
+
+
+c = Client()
+c.user_loop()
