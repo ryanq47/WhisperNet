94eea624ce0c017a63c047839288f307ce7ce17f
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2023-12-05 23:30:21 -0600

    Simplec2 updates

--- GUI/QtComponents/SimpleC2/simplec2.py
+++ GUI/QtComponents/SimpleC2/simplec2.py
@@ -1,9 +1,14 @@
 from PySide6.QtWidgets import QWidget, QTextEdit, QMessageBox, QTreeWidgetItem, QTreeWidget, QPushButton, QMenu
 from PySide6.QtGui import QIcon
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import Qt
+from PySide6.QtNetwork import QNetworkReply
+from PySide6.QtCore import Qt, Signal
+from functools import partial
+from Utils.QtWebRequestManager import WebRequestManager
+import json
 
 class Simplec2(QWidget):
+    signal_get_client_data= Signal(str)
     def __init__(self):
         super().__init__()
         
@@ -13,6 +18,7 @@ class Simplec2(QWidget):
 
         self.__ui_load()
         self.name = "SimpleC2"
+
 
     def __ui_load(self):
         '''
@@ -32,7 +38,15 @@ class Simplec2(QWidget):
             self.client_tree = self.ui_file.findChild(QTreeWidget, "client_tree_widget")
             
             self.add_client = self.ui_file.findChild(QPushButton, "add_client")
-            self.add_client.clicked.connect(self.add_client_to_widget)
+            self.refresh_manually = self.ui_file.findChild(QPushButton, "refresh_manually")
+
+
+            # temp
+            data_dict = None
+            self.add_client.clicked.connect(partial(self.add_client_to_widget,data_dict))
+
+            self.refresh_manually.clicked.connect(partial(self.get_client_data))
+
 
             ## MUST GO LAST!!!
             self.setLayout(self.ui_file.layout())
@@ -76,21 +90,110 @@ class Simplec2(QWidget):
         msg.setIcon(QMessageBox.Information)
         msg.exec_()
 
-    def add_client_to_widget(self, data_dict = None):
+    def add_client_to_widget(self, json_data = None):
         '''
         Adds a host to the GUI widget/window
         
-        data_dict will hold data needed to populate the entry (eventually)
+        json_data will hold data needed to populate the entry (eventually)
+        json_data: A json dict. Gets turned into a pyobject
         '''
-        ## Main entry
-        client_entry = QTreeWidgetItem(["Item 1", "This is item 1"])
-        client_entry.setIcon(0, QIcon("Assets/client.png"))
-        self.client_tree.addTopLevelItem(client_entry)
+        #print(data_dict)
 
-        ## Sub Items
-        #                         parent,        stuff         stuff
-        subItem = QTreeWidgetItem(client_entry, ["Subitem 1", "This is subitem 1"])
-        #subItem.setIcon(0, QIcon("path/to/icon.png"))#if you want an icon
-        # Check CSS for more icon stuff, under : /*QTree Item thingy*/
-        ## force expanded
-        #client_entry.setExpanded(True)
+        ## Flip to obj
+        data_dict = json.loads(json_data)
+        
+        if data_dict == None:
+            data_dict = {
+                "client":"ERROR",
+                "ip":"",
+                "port":"",
+                "listener":"",
+                "sleep":"",
+                "username":"",
+                "client_type":""
+            }
+
+        #print(data_dict)
+
+        for data in data_dict:
+            print(data)
+            #print("data_dict:", data_dict, "type:", type(data_dict))
+            ## Main entry
+            client_entry = QTreeWidgetItem([data["client"], data["ip"], 
+                                            data["port"], data["listener"], 
+                                            data["sleep"]])
+            
+            client_entry.setIcon(0, QIcon("Assets/client.png"))
+            self.client_tree.addTopLevelItem(client_entry)
+
+            ## Sub Items
+            #                         parent,        stuff         stuff
+            username_subitem = QTreeWidgetItem(client_entry, ["User:", data["username"]])
+            clienttype_subitem = QTreeWidgetItem(client_entry, ["Client Type:", data["client_type"]])
+
+            #subItem.setIcon(0, QIcon("path/to/icon.png"))#if you want an icon
+            # Check CSS for more icon stuff, under : /*QTree Item thingy*/
+            ## force expanded
+            #client_entry.setExpanded(True)
+        
+        self.update_client_tree_options()
+
+    def get_client_data(self):
+        '''
+        Communicates with server to get client data. 
+
+        Eventually will be run in a qtimer loop thingymabober
+        '''
+        ## have to use this for async communication/to fit QT
+        self.request_manager = WebRequestManager()
+
+        #post request for now. Subject to chagne
+        self.request_manager.send_get_request(
+            url = "https://67ea750d-d74e-490d-ac09-0167215b8b23.mock.pstmn.io/simplec2/clients",
+            #data = ""
+        )
+
+        self.request_manager.request_finished.connect(
+            ##  send request                                      ## Signal that holds data
+            lambda response: self.handle_response(response, self.signal_get_client_data)
+        )## I don't fully remeber why we have to pass response to the function as well. 
+
+        # when signal is triggerd, called add_client_to_widget
+        self.signal_get_client_data.connect(self.add_client_to_widget)
+        # Once we have data, loop call add_client_to_widget, or make it auto loop? not sure.
+        # May not have to worry about indexing items as QT will (should) have options for that. 
+
+    def update_client_tree_options(self):
+        '''
+        Does any updates tot he client tree, such as resizing cells, etc
+        '''
+
+        for column in range(self.client_tree.columnCount()):
+            self.client_tree.resizeColumnToContents(column)
+
+    def handle_response(self, reply, signal):        
+        '''
+        Handles a web request response. 
+
+        Emits a SIGNAL, named 'response_received'. This signal 
+        contains the web request response. 
+        
+        This will trigger signals in the respective functions that called it.
+        
+
+        reply: web reply
+        signal: The signal to return/emit to. this is so signals dont have the same data passed between them 
+        causing issues
+
+        '''
+        #logging.debug(f"{function_debug_symbol} {inspect.stack()[0][3]}")
+        try:
+            if reply.error() == QNetworkReply.NoError:
+                response_data = reply.readAll().data().decode()
+                signal.emit(response_data)  # Emit the custom signal
+            else:
+                string = f"Error with request: {reply.error()}"
+                signal.emit(string)  # Emit the custom signal
+        except Exception as e:
+            print(e)
+            #logging.debug(f"{function_debug_symbol} Error with handle_response: {e}")
---
+++ GUI/Utils/QtWebRequestManager.py
@@ -0,0 +1,34 @@
+from PySide6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from PySide6.QtCore import Signal
+
+class WebRequestManager(QNetworkAccessManager):
+    request_finished = Signal(QNetworkReply)
+
+    def send_post_request(self, url, data):
+        #logging.debug(f"{function_debug_symbol} {inspect.stack()[0][3]}")
+        try:
+            request = QNetworkRequest(url)
+            request.setHeader(QNetworkRequest.ContentTypeHeader, "application/json")
+            #application/octet-stream
+            #request.setHeader(QNetworkRequest.ContentTypeHeader, "application/octet-stream")
+
+            reply = self.post(request, data)
+            reply.finished.connect(self.handle_response)
+        except Exception as e:
+            print(e)
+            #logging.warning(f"{function_debug_symbol} Error sending post request: {e}")
+
+    def send_get_request(self, url):
+        #logging.debug(f"{function_debug_symbol} {inspect.stack()[0][3]}")
+
+        request = QNetworkRequest(url)
+        request.setHeader(QNetworkRequest.ContentTypeHeader, "application/json")
+        
+        reply = self.get(request)
+        reply.finished.connect(self.handle_response)
+
+    def handle_response(self):
+        #logging.debug(f"{function_debug_symbol} {inspect.stack()[0][3]}")
+
+        reply = self.sender()
+        self.request_finished.emit(reply)
